76bde47eaa5d3a8b56ea477e0d521487e0d6aa1a
Volodymyr Labenskyi <v.labenskyi@sana-commerce.com>
2025-04-21 12:06:34 +0300

    TabButton as a separate component

---
+++ src/app/components/layout/TabButton.tsx
@@ -0,0 +1,28 @@
+import type { TextKey } from "@/app/behavior/translations";
+import { useTranslations } from "../language";
+
+type Props = {
+  labelKey: TextKey,
+  onClick: () => void;
+  isActiveTab: boolean;
+};
+
+const TabButton = ({ labelKey, onClick, isActiveTab }: Props) => {
+  const { t } = useTranslations();
+
+  return (
+    <button
+      onClick={onClick}
+      className={`px-4 py-2 font-semibold rounded-t-lg focus:outline-none transition-colors duration-200 ease-in-out border-b-2 ${
+        isActiveTab
+          ? "bg-background-list text-text-primary border-accent"
+          : "bg-background-header text-text-secondary border-transparent hover:bg-background-hover hover:text-text-primary"
+      }`}
+    >
+      {" "}
+      {t(labelKey)}{" "}
+    </button>
+  );
+};
+
+export default TabButton;
