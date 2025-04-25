54a308e4b56ca0516e918c95b8bf522c3acdb651
Volodymyr Labenskyi <v.labenskyi@sana-commerce.com>
2025-04-25 22:20:01 +0300

    Svg formatting

--- src/app/components/icons/SettingsIcon.tsx
+++ src/app/components/icons/SettingsIcon.tsx
@@ -3,7 +3,8 @@ const SettingsIcon = () => (
     <path
       fillRule="evenodd"
       d="M2 4.75A.75.75 0 0 1 2.75 4h14.5a.75.75 0 0 1 0 1.5H2.75A.75.75 0 0 1 2 4.75ZM2 10a.75.75 0 0 1 .75-.75h14.5a.75.75 0 0 1 0 1.5H2.75A.75.75 0 0 1 2 10Zm0 5.25a.75.75 0 0 1 .75-.75h14.5a.75.75 0 0 1 0 1.5H2.75a.75.75 0 0 1-.75-.75Z"
-      clipRule="evenodd" />
+      clipRule="evenodd"
+    />
   </svg>
 );
 
