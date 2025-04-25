860fa0b869bed22277ac212fa82d8512e54b773d
Volodymyr Labenskyi <v.labenskyi@sana-commerce.com>
2025-04-25 22:18:50 +0300

    Fix border radius for tab buttons in mobile

--- src/app/components/layout/TabButton.tsx
+++ src/app/components/layout/TabButton.tsx
@@ -13,7 +13,7 @@ const TabButton = ({ labelKey, onClick, isActiveTab }: Props) => {
   return (
     <button
       onClick={onClick}
-      className={`px-4 py-2 font-semibold rounded-t-lg focus:outline-none transition-colors duration-200 ease-in-out border-b-2 w-full sm:w-auto ${
+      className={`px-4 py-2 font-semibold first:rounded-t-lg md:rounded-t-lg focus:outline-none transition-colors duration-200 ease-in-out border-b-2 w-full sm:w-auto ${
         isActiveTab
           ? "bg-background-header text-text-primary border-accent"
           : "bg-background-list text-text-secondary border-transparent hover:bg-background-hover hover:text-text-primary"
