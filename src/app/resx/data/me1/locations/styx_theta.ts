8d6a4ab33c81ce6669ee390edb45652849ae31b9
Volodymyr Labenskyi <v.labenskyi@sana-commerce.com>
2025-06-03 13:54:33 +0300

    Styx Theta discovery locations

--- src/app/resx/data/me1/locations/styx_theta.ts
+++ src/app/resx/data/me1/locations/styx_theta.ts
@@ -4,9 +4,11 @@ import * as m from '../missions';
 // Styx Theta
 
 export const me1_location_styx_theta_acheron_carbonaceous_asteroid = loc('Carbonaceous Asteroid')
+  .hasMissions(m.me1_mission_turian_insignias)
   .build();
 
 export const me1_location_styx_theta_acheron_grosalgen = loc('Grosalgen')
+  .hasMissions(m.me1_mission_valuable_minerals_light_metals)
   .build();
 
 export const me1_location_styx_theta_acheron_altahe = loc('Altahe')
@@ -22,9 +24,11 @@ export const me1_system_styx_theta_acheron = sys('Acheron')
   .build();
 
 export const me1_location_styx_theta_erebus_quaji = loc('Quaji')
+  .hasMissions(m.me1_mission_valuable_minerals_heavy_metals)
   .build();
 
 export const me1_location_styx_theta_erebus_wermani = loc('Wermani')
+  .hasMissions(m.me1_mission_prothean_data_discs)
   .build();
 
 export const me1_location_styx_theta_erebus_nepmos = loc('Nepmos')
