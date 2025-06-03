072bc94eaee7fd5860cd52544056c31b7b063e6b
Volodymyr Labenskyi <v.labenskyi@sana-commerce.com>
2025-06-03 13:16:47 +0300

    Horse Head Nebula discovery missions

--- src/app/resx/data/me1/locations/horse_head_nebula.ts
+++ src/app/resx/data/me1/locations/horse_head_nebula.ts
@@ -4,9 +4,11 @@ import * as m from '../missions';
 // Horse Head Nebula
 
 export const me1_location_horse_head_nebula_fortuna_maganlis = loc('Maganlis')
+  .hasMissions(m.me1_mission_turian_insignias)
   .build();
 
 export const me1_location_horse_head_nebula_fortuna_therumlon = loc('Therumlon')
+  .hasMissions(m.me1_mission_valuable_minerals_rare_metals)
   .build();
 
 export const me1_location_horse_head_nebula_fortuna_amaranthine = loc('Amaranthine')
@@ -22,21 +24,30 @@ export const me1_system_horse_head_nebula_fortuna = sys('Fortuna')
   .build();
 
 export const me1_location_horse_head_nebula_pax_veles = loc('Veles')
+  .hasMissions(m.me1_mission_asari_writings)
+  .build();
+
+export const me1_location_horse_head_nebula_pax_noveria = loc('Noveria')
+  .hasMissions(m.me1_mission_geth_interest)
   .build();
 
 export const me1_system_horse_head_nebula_pax = sys('Pax')
   .hasLocations(
     me1_location_horse_head_nebula_pax_veles,
+    me1_location_horse_head_nebula_pax_noveria,
   )
   .build();
 
 export const me1_location_horse_head_nebula_strenuus_yunthorl = loc('Yunthorl')
+  .hasMissions(m.me1_mission_locate_signs_of_battle)
   .build();
 
 export const me1_location_horse_head_nebula_strenuus_antitarra = loc('Antitarra')
+  .hasMissions(m.me1_mission_valuable_minerals_gasses)
   .build();
 
 export const me1_location_horse_head_nebula_strenuus_msv_majesty = loc('MSV Majesty')
+  .hasMissions(m.me1_mission_privateers)
   .build();
 
 export const me1_location_horse_head_nebula_strenuus_xawin = loc('Xawin')
