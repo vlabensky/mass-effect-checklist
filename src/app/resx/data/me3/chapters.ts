a90178ef32e33aac3adfe6622ac58481b311b7f7
Volodymyr Labenskyi <v.labenskyi@sana-commerce.com>
2025-06-04 17:21:43 +0300

    Main plot missions

--- src/app/resx/data/me3/chapters.ts
+++ src/app/resx/data/me3/chapters.ts
@@ -8,5 +8,25 @@ export const me3_chapter_main_plot = ch('Main Plot')
     m.me3_mission_prologue_earth,
     m.me3_mission_priority_mars,
     m.me3_mission_priority_the_citadel_1,
+    m.me3_mission_priority_palaven,
+    m.me3_mission_priority_surkesh,
+    m.me3_mission_priority_tuchanka,
+    m.me3_mission_priority_citadel_2,
+    m.me3_mission_priority_perseus_veil,
+    m.me3_mission_priority_geth_dreadnought,
+    m.me3_mission_priority_rannoch,
+    m.me3_mission_priority_citadel_3,
+    m.me3_mission_priority_thessia,
+    m.me3_mission_priority_horizon,
+    m.me3_mission_priority_cerberus_hq,
+    m.me3_mission_priority_earth,
   )
   .build();
+
+// Post-Citadel I Missions
+
+export const me3_chapter_post_citadel_1 = ch('Post-Citadel I Missions')
+  .hasMissions(
+  )
+  .build();
+
--- src/app/resx/data/me3/missions.ts
+++ src/app/resx/data/me3/missions.ts
@@ -1,6 +1,6 @@
 import { m } from '../builders';
 
-// Act I
+// Main Plot
 
 export const me3_mission_prologue_earth = m('Prologue: Earth', 'Prologue:_Earth', 'Prologue_Earth')
   .build();
@@ -9,4 +9,40 @@ export const me3_mission_priority_mars = m('Priority: Mars', 'Priority:_Mars', '
   .build();
 
 export const me3_mission_priority_the_citadel_1 = m('Priority: The Citadel I', 'Priority:_The_Citadel_I', 'Priority_The_Citadel_1')
+  .build();
+
+export const me3_mission_priority_palaven = m('Priority: Palaven', 'Priority:_Palaven', 'Priority_Palaven')
+  .build();
+
+export const me3_mission_priority_surkesh = m('Priority: Sur\'Kesh', 'Priority:_Sur%27Kesh', 'Priority_SurKesh')
+  .build();
+
+export const me3_mission_priority_tuchanka = m('Priority: Tuchanka', 'Priority:_Tuchanka', 'Priority_Tuchanka')
+  .build();
+
+export const me3_mission_priority_citadel_2 = m('Priority: The Citadel II', 'Priority:_The_Citadel_II', 'Priority_The_Citadel_2')
+  .build();
+
+export const me3_mission_priority_perseus_veil = m('Priority: Perseus Veil', 'Priority:_Perseus_Veil', 'Priority_Perseus_Veil')
+  .build();
+
+export const me3_mission_priority_geth_dreadnought = m('Priority: Geth Dreadnought', 'Priority:_Geth_Dreadnought', 'Priority_Geth_Dreadnought')
+  .build();
+
+export const me3_mission_priority_rannoch = m('Priority: Rannoch', 'Priority:_Rannoch', 'Priority_Rannoch')
+  .build();
+
+export const me3_mission_priority_citadel_3 = m('Priority: The Citadel III', 'Priority:_The_Citadel_III', 'Priority_The_Citadel_3')
+  .build();
+
+export const me3_mission_priority_thessia = m('Priority: Thessia', 'Priority:_Thessia', 'Priority_Thessia')
+  .build();
+
+export const me3_mission_priority_horizon = m('Priority: Horizon', 'Priority:_Horizon', 'Priority_Horizon')
+  .build();
+
+export const me3_mission_priority_cerberus_hq = m('Priority: Cerberus Headquarters', 'Priority:_Cerberus_Headquarters', 'Priority_Cerberus_HQ')
+  .build();
+
+export const me3_mission_priority_earth = m('Priority: Earth', 'Priority:_Earth', 'Priority_Earth')
   .build();
