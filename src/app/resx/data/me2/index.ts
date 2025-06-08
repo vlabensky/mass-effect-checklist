6d5f8b1f323df4b02416d446eccdb93ed5c7c757
Volodymyr Labenskyi <v.labenskyi@sana-commerce.com>
2025-06-08 16:44:55 +0300

    Url providers built for missions

--- src/app/resx/data/me2/index.ts
+++ src/app/resx/data/me2/index.ts
@@ -2,12 +2,26 @@ import type { GameData } from '../types';
 import * as m from './missions';
 import * as chapters from './chapters';
 import * as clusters from './locations';
+import { urlProviders } from './urls';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const { predicates, ...missions } = m;
 
-export const me2_game_data: GameData = {
+const me2_game_data: GameData = {
   chapters: Object.values(chapters),
   missions: Object.values(missions),
   clusters: Object.values(clusters),
 };
+
+urlProviders.forEach(({ name: providerName, missionUrls }) => {
+  for (const [mission_id, mission] of Object.entries(missions)) {
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const url = Object.entries(missionUrls).find(([missionUrlId, _]) => missionUrlId === mission_id)![1];
+    mission.urls.push({
+      name: providerName,
+      url,
+    });
+  }
+});
+
+export { me2_game_data };
