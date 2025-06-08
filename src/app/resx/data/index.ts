4d5ef7bf52381ce3e9c926aeecac237a365bf804
Volodymyr Labenskyi <v.labenskyi@sana-commerce.com>
2025-06-08 17:25:25 +0300

    Export games from data

--- src/app/resx/data/index.ts
+++ src/app/resx/data/index.ts
@@ -2,7 +2,7 @@ import { me1_game_data } from './me1';
 import { me2_game_data } from './me2';
 import { me3_game_data } from './me3';
 
-export const game_data = {
+export const games = {
   me1: me1_game_data,
   me2: me2_game_data,
   me3: me3_game_data,
